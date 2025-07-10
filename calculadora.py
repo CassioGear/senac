1b8e868ddb1e0462c78b2cf5ccbbaf124f1b0164
CassioGear <cassianovalentim2009@gmail.com>
2025-07-10 10:07:50 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite um número: '))
+c = int(input('Digite um número: '))
+d = int(input('Digite um número: '))
+e = int(input('Digite um número: '))
+f = int(input('Digite um número: '))
+g = int(input('Digite um número: '))
+h = int(input('Digite um número: '))
+multiplicacao = e * f
+subtracao = c - d
+soma = a + b
+divisao = g / h
+print('A Soma é '+str(soma))
+print('A Subtração é '+str(subtracao))
+print('A Multiplicaçao é '+str(multiplicacao))
+print('A Divisão é '+str(divisao))
+
