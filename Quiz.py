4fb4382f97adeb543825c46439db6f2eba17cfb3
CassioGear <cassianovalentim2009@gmail.com>
2025-08-07 10:28:02 -0300

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,36 @@
+print("Bleach Quiz")
+print("Quem é o atual Capitão-Comandante da Gotei 13?")
+a = input("R: ")
+if a == "Shunsui" or "Kyoraku":
+    print("Resposta Correta!")
+else:
+    print("Errado!")
+print(=================)
+print("Qual é a verdadeira identidade do Velho Zangetsu?")
+a = input("R: ")
+if a == "Yhwach"
+ print("Resposta Correta!")
+else:
+    print("Errado!")
+print(=================)
+print("Qual o Espada mais forte do exército de Aizen?")
+a = input("R: ")
+if a == "Starrk"
+ print("Resposta Correta!")
+else:
+    print("Errado!")
+print(=================)
+print("Quem é o irmão de Rukia?")
+a = input("R: ")
+if a == "Byakuya"
+ print("Resposta Correta!")
+else:
+    print("Errado!")
+print(=================)
+print("Uryu é um?")
+a = input("R: ")
+if a == "Quincy"
+ print("Resposta Correta!")
+else:
+    print("Errado!")
+print(=================)
